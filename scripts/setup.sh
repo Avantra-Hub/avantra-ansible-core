408cdad96fc1fb81aa0d2ab030af1f2a99086da6
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-10 15:43:42 +0100

    XAN-3344: build pipeline

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-# apt install python-is-python3
+apt update && apt install -y software-properties-common
 add-apt-repository ppa:deadsnakes/ppa
+# apt install python-is-python3
 apt update && apt install -y ansible python3.9
