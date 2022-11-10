2486f02b21b99ecb959a32e06f2b25516519478c
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-10 20:44:02 +0100

    XAN-3344: prerequisites

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ mkdir -p /tests-"$1"/ansible_collections/avantra
 cp -r . /tests-"$1"/ansible_collections/avantra/core
 cd /tests-"$1"/ansible_collections/avantra/core
 python -m venv .venv
+echo "Hello1!"
 source .venv/bin/activate
 echo "Hello!"
 deactivate
