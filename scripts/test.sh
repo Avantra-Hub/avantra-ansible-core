e279aaa969c5eb8079bbc608d9e0073c4808193f
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-10 21:50:46 +0100

    XAN-3344: prerequisites

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,6 +12,16 @@ else
 fi
 
 ansible-test sanity --junit --python "$1"
+
+//It contains the status of the last executed command
+if [[ $? -eq 0 ]]; then
+  echo "success*********"
+  exit 0
+else
+  echo "failed************"
+  exit 1
+fi
+
 ansible-test units --coverage --python "$1" --requirements
 ansible-test coverage xml --requirements
 
