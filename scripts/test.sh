f5e28d81df5d5b6c83c0fa95af6890e5b9a32fe5
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-10 15:14:13 +0100

    XAN-3344: build pipeline

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ pwd
 ls -la
 mkdir -p ./ansible_collections/avantra
 ln -s . ./ansible_collections/avantra/core
-cd ./ansible_collections/avantra/core/ || exit
+cd ./ansible_collections/avantra/core/
 pwd
 ls -la
 /usr/bin/ansible-test sanity
