820fefdfa15073c7abe5c3776d04f98ce8008346
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-10 15:52:21 +0100

    XAN-3344: build pipeline

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-apt update && apt install -y software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-# apt install python-is-python3
-apt update && apt install -y ansible python3.9
+apt update && apt install -y ansible
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-python --version
-python3 --version
 cp -r . core
 mkdir -p ansible_collections/avantra
 mv core ansible_collections/avantra
 cd ansible_collections/avantra/core
-/usr/bin/ansible-test sanity
+/usr/bin/ansible-test sanity --docker
