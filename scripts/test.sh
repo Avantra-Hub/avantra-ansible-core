c1fac2b73412426b21bf7d26c6e2965ef55b5859
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-10 15:00:15 +0100

    XAN-3344: build pipeline

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mkdir -p /ansible_collections/avantra
-ln -s /ansible_collections/avantra/core .
+mkdir -p ./ansible_collections/avantra
+ln -s ./ansible_collections/avantra/core .
+cd ./ansible_collections/avantra/core || exit
 /usr/bin/ansible-test sanity
