3817f7ac43668545af7ac279790179f60e6247a8
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-10 21:07:44 +0100

    XAN-3344: prerequisites

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,15 @@
 mkdir -p /tests-"$1"/ansible_collections/avantra
 cp -r . /tests-"$1"/ansible_collections/avantra/core
 cd /tests-"$1"/ansible_collections/avantra/core
-python -m pip install ansible "ansible-core>=$1,<$2"
-ansible --version
 
+if [ -z "$2" ]
+  then
+    python -m pip install ansible "ansible-core>=$1"
+else
+  python -m pip install ansible "ansible-core>=$1,<$2"
+fi
+
+ansible-test coverage erase
+ansible-test sanity --junit
+ansible-test units --coverage
+ansible-test coverage xml
