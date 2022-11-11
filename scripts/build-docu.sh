3b972371af69cce970051d1d8f2a3879cd6936b1
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-11 09:38:33 +0100

    Build the documentation.

--- scripts/build-docu.sh
+++ scripts/build-docu.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo Build documentation
+mkdir -p /root/ansible-docu
+cd /root/ansible-docu
+
+git clone https://github.com/ansible-community/antsibull-core.git
+git clone https://github.com/ansible-community/antsibull-docs.git
+cd antsibull-docs
+python3 -m pip install poetry
+poetry install
+
+mkdir dest
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+./build.sh
+
+ls -la dest
