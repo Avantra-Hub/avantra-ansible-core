b89a19fcc0401ad2580c7346c95cdcc9965b5bb9
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-11 09:52:05 +0100

    Build the documentation.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,12 @@ mkdir -p /build-"$1"/ansible_collections/avantra
 cp -r . /build-"$1"/ansible_collections/avantra/core
 cd /build-"$1"/ansible_collections/avantra/core
 
-python -m pip install "ansible-core>=$1"
+python -m pip install "ansible-core>=$1" yq
 
 ansible-galaxy collection build
 
+version=$(yq -r .version galaxy.yml)
+echo "**************************************** $version"
+
 ls -la
 
-echo "****************************************"
