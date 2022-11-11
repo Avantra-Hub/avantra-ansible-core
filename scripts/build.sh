1010f5dfa159038ea68118ef32152ec544756006
Michael Szediwy <michael.szediwy@avantra.com>
2022-11-11 09:35:01 +0100

    Update the sanity test ignore file for Ansible 2.15.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo Build
+mkdir -p /build-"$1"/ansible_collections/avantra
+cp -r . /build-"$1"/ansible_collections/avantra/core
+cd /build-"$1"/ansible_collections/avantra/core
+
+python -m pip install "ansible-core>=$1"
+
+ansible-galaxy collection build
+
+ls -la
