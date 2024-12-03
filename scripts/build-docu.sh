fff48609dcc68e0b1c14916889eed95b1ba75c38
Michael Szediwy <michael.szediwy@avantra.com>
2024-12-03 12:03:24 +0000

    Upgrade the version string

--- scripts/build-docu.sh
+++ scripts/build-docu.sh
@@ -11,41 +11,36 @@ EOF
 )
 
 apt-get update && apt-get install rsync -y
-
-python -m pip install "ansible-core>=$1"
-
-ls -la
+python3 -m pip install "ansible-core>=$1"
 ansible-galaxy collection install build/avantra-core-"$version".tar.gz
 
-mkdir -p /root/ansible-docu
-cd /root/ansible-docu || { echo "cd /root/ansible-docu impossible"; exit 1; }
+mkdir -p  $current_dir/target/ansible-docu
+cd $current_dir//target/ansible-docu || { echo "cd $current_dir//target/ansible-docu impossible"; exit 1; }
 
-git clone https://github.com/ansible-community/antsibull-core.git
-git clone https://github.com/ansible-community/antsibull-docs.git
-cd antsibull-docs || { echo "cd antsibull-docs impossible"; exit 1; }
-python3 -m pip install poetry==1.2.2
-poetry install
+python3 -m venv .antsibull-docs-venv
+. .antsibull-docs-venv/bin/activate
+python -m pip install ansible-core antsibull-docs
+
 mkdir dest
-chmod 644 dest
-poetry run antsibull-docs sphinx-init --use-current --dest-dir dest avantra.core
-chmod -R 644 dest
+antsibull-docs sphinx-init --use-current --dest-dir dest avantra.core
 cd dest || { echo "cd dest impossible"; exit 1; }
 dest_dir=$(pwd)
-#python3 -m pip install -r requirements.txt
-set -e
-cd "$dest_dir" || { echo "cd $dest_dir impossible"; exit 1; }
+python -m pip install -r requirements.txt
+
+echo "1. ************************************************************************************"
 
 mkdir temp-rst
-chmod -R 744 .
+chmod -R 700 temp-rst
 antsibull-docs \
-    --config-file antsibull-docs.cfg \
     collection \
     --use-current \
     --dest-dir temp-rst \
     avantra.core
 
 # Copy collection documentation into source directory
+echo "2. ************************************************************************************"
 rsync -cprv --delete-after temp-rst/collections/ rst/collections/
+echo "3. ************************************************************************************"
 
 # Build Sphinx site
 sphinx-build -M html rst build -c . -W --keep-going
